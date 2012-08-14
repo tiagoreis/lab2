8ede73ad4527663d67781dda085a2a0343867a97
tiagoreis <tiagooreis@gmail.com>
2012-08-13 21:47:41 -0300

    teste em aula com classe pessoa

---
+++ entregas/exercicio2/src/main/java/br/com/senac/alp/aulas/exercicio2/Pessoa.java
@@ -0,0 +1,20 @@
+package br.com.senac.alp.aulas.exercicio2;
+
+public class Pessoa {
+
+	//criacao com inicializacao de valor 
+	private String nome = null;
+
+	//usado o source/ generate
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+}
