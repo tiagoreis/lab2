09f2c6804dcad1eb01a398bf91c374e2d97cd1fc
tiagoreis <tiagooreis@gmail.com>
2012-08-13 22:10:43 -0300

    classe funcionario

---
+++ entregas/exercicio2/src/main/java/br/com/senac/alp/aulas/exercicio2/Funcionario.java
@@ -0,0 +1,17 @@
+package br.com.senac.alp.aulas.exercicio2;
+
+public class Funcionario extends Pessoa {
+
+	private Integer id = null;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+}
--- entregas/exercicio2/src/main/java/br/com/senac/alp/aulas/exercicio2/Pessoa.java
+++ entregas/exercicio2/src/main/java/br/com/senac/alp/aulas/exercicio2/Pessoa.java
@@ -5,7 +5,7 @@ public class Pessoa {
 	//criacao com inicializacao de valor 
 	private String nome = null;
 
-	//usado o source/ generate
+	//usado o source/generate get an set
 	public String getNome() {
 		return nome;
 	}
