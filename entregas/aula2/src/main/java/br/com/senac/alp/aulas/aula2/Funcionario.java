a0e66709780c12c677cf70b17f8f8553c17ea331
tiagoreis <tiagooreis@gmail.com>
2012-08-13 22:19:08 -0300

    primeiro commit

---
+++ entregas/aula2/src/main/java/br/com/senac/alp/aulas/aula2/Funcionario.java
@@ -0,0 +1,17 @@
+package br.com.senac.alp.aulas.aula2;
+
+public class Funcionario extends Pessoa {
+
+	private Integer id = null;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+}
---
+++ entregas/aula2/src/main/java/br/com/senac/alp/aulas/aula2/Pessoa.java
@@ -0,0 +1,20 @@
+package br.com.senac.alp.aulas.aula2;
+
+public class Pessoa {
+
+	//criacao com inicializacao de valor 
+	private String nome = null;
+
+	//usado o source/generate get an set
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+}
