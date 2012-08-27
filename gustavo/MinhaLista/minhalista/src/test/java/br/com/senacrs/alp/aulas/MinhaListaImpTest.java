1bffe3d6d9f613ea90f5f0f45ea21950b2f1c381
tiagoreis <tiagooreis@gmail.com>
2012-08-27 20:06:32 -0300

    codigo gustavo

---
+++ gustavo/MinhaLista/minhalista/src/test/java/br/com/senacrs/alp/aulas/MinhaListaImpTest.java
@@ -0,0 +1,54 @@
+package br.com.senacrs.alp.aulas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinhaListaImpTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMinhaListaImp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSufixar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrefixar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInserir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemover() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTamanho() {
+		fail("Not yet implemented");
+	}
+
+}
