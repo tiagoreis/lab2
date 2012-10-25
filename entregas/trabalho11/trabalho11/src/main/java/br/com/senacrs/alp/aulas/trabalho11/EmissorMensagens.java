1911d7eecf89037f22084db933b309fd4a12ab04
tiago <tiago>
2012-10-25 16:20:37 -0200

    trabalho: ler arquivo / validar chaves/ alterar palavras em um map

--- entregas/trabalho11/trabalho11/src/main/java/br/com/senacrs/alp/aulas/trabalho11/EmissorMensagens.java
+++ entregas/trabalho11/trabalho11/src/main/java/br/com/senacrs/alp/aulas/trabalho11/EmissorMensagens.java
@@ -1,118 +1,132 @@
 package br.com.senacrs.alp.aulas.trabalho11;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.StringTokenizer;
-
 
 public class EmissorMensagens {
 
 	File arquivo = null;
+	String token = "=";
 
-	
+	BufferedReader entrada;
+	BufferedWriter saida;
+	Map<String, String> mapa = new HashMap<String, String>();
+
 	private Map<String, String> map = new HashMap<String, String>();
-	
+
 	public EmissorMensagens(String nomeArquivo) {
 
-		BufferedReader reader = null;
-		FileReader file = null;
-		String linhas = null;
-		int contadorChave = 0;
-		StringTokenizer stringToken = null;
-		String token = "=";
-		LinkedList<String> linhasLista = new LinkedList<String>();
-		String [] arrayChaveValor = null;
-		
 		if (nomeArquivo == null) {
 			throw new IllegalArgumentException();
 		}
 
 		arquivo = new File(nomeArquivo);
 
-		if (arquivo.isDirectory()) {
+		if (arquivo.isDirectory() || !arquivo.exists()) {
 			throw new IllegalArgumentException();
 		}
+
+		validaArquivo();
+
+	}
+
+	
+	
+	@SuppressWarnings("static-access")
+	public String formatarMensagem(String chave, Object... argumentos) {
+
+		if (map.get(chave) == null) {
+			throw new IllegalArgumentException();
+		}
+
+		String chaveMap = null;
+		String textoFormatado = null;
+		
+		chaveMap = map.get(chave);
+		textoFormatado = chaveMap.format(chaveMap, argumentos);
+
+//		System.out.println(chaveMap );
+//		System.out.println(textoFormatado);
+		
+		return textoFormatado.trim();
+
+	}
+
+	
+	
+	public void validaArquivo() {
 
 		if (!arquivo.exists()) {
 			throw new IllegalArgumentException();
 		}
 
-		
-		
+		String linhas = null;
+		String key = null;
+		String value = null;
+		int contadorChave = 0;
+		StringTokenizer stringToken = null;
+
+		BufferedReader reader = null;
+
 		try {
-			
-			file = new FileReader(arquivo);
-			reader = new BufferedReader(file);
-			linhas = reader.readLine() ;
-			
-			
-			while(linhas != null){
-				
-				contadorChave = 0;
-				
-				System.out.println("*****"+linhas+"*****");
-				
-				
-				if(linhas.indexOf(token, 1) == -1){
-					System.out.println("nao tem token " + linhas);
-					throw new IllegalArgumentException("nao tem token");
-				}
-				
-				
-				
-				stringToken = new StringTokenizer(linhas, token);
-				contadorChave = stringToken.countTokens();
-				
-				if(contadorChave != 2 ){
-					//System.out.println("cont "+contadorChave);
-					throw new IllegalArgumentException("erro no token");
-				}
-				
-				
 
-				
-				//arrayChaveValor  = linhas.split(token);
-				//System.out.println("arr " + arrayChaveValor[0]);
-				//System.out.println("arr " + arrayChaveValor[1]);
-				//map.put(key, value)
-				
-				
-				//linhasLista.add(linhas);
-				
-				linhas = reader.readLine();
-			
+			reader = new BufferedReader(new FileReader(arquivo));
+
+			try {
+				while ((linhas = reader.readLine()) != null) {
+	
+					// System.out.println(linhas);
+
+					stringToken = new StringTokenizer(linhas, token);
+					contadorChave = stringToken.countTokens();
+
+					// se nao tiver separador
+					if (linhas.indexOf(token) == -1) {
+
+						throw new IllegalArgumentException();
+					} else {
+
+						// System.out.println(linhas);
+
+						// se tiver mais de um separador
+						if (contadorChave > 2) {
+							throw new IllegalArgumentException();
+						} else {
+
+							
+							// tira os sacanas dos espaços duplos
+							key = linhas.substring(0,
+									linhas.indexOf(" " + token));
+							value = linhas.substring(linhas
+									.indexOf(token + " ") + 2);
+
+							
+							map.put(key, value);
+							// System.out.println("----------------------------------");
+
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			
-		} catch (Exception e) {
-			//e.printStackTrace();
-			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-
-		//System.out.println(nomeArquivo);
-
 	}
 
-	public String formatarMensagem(String chave, Object... argumentos) {
 
-		if (argumentos.length == 0) {
-			throw new IllegalArgumentException();
-		}
 
-		if (chave == null) {
-			throw new IllegalArgumentException();
-		}
-
-		//System.out.println(chave);
-		
-		return null;
-	}
-	
-		
-	
 }
