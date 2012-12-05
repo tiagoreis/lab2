b62422063f49ecba9b41c5a5de9410ebb0fa4941
tiago <tiago@PROD-J03.zeta.dominio>
2012-12-05 09:05:52 -0200

    refactoring

--- entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
+++ entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
@@ -8,115 +8,119 @@ import java.util.List;
 public class ListaDiretorio {
 
 	public String[] listaConteudoDiretorio(String diretorio) {
-		
+
+		validaDiretorio(diretorio);
+
 		String[] resultado = null;
-		String[] dir = null;
-		String[] arq = null;
-		
-		String newLine = System.getProperty("line.separator");
-		String name = null;
-		
-		int numeroArquivos= 0;
-		int numeroDiretorios=0;
-		int contador=0;
-		int cont = 0;
-		
-		if(diretorio == null){
-			throw new IllegalArgumentException();
-		}
 
-		
+		resultado = ordenaConteudoDiretorio(diretorio);
+
+		return resultado;
+	}
+
+	private String[] ordenaConteudoDiretorio(String diretorio) {
+
 		File fileDiretorio = new File(diretorio);
-		
-		if(!fileDiretorio.isDirectory()){
-			throw new IllegalArgumentException();
-		}
-
 		File[] listFile = fileDiretorio.listFiles();
 
-		if(listFile.length > 0){
+		String[] resultado = null;
+		String[] arrayDiretorios = null;
+		String[] arrayArquivos = null;
 
-			contador = listFile.length ;
+		int numeroArquivos = 0;
+		int numeroDiretorios = 0;
+
+		resultado = new String[listFile.length + 2];
+		resultado[0] = "list: " + fileDiretorio;
+		resultado[1] = "total: " + listFile.length;
+
+		if (listFile.length > 0) {
+
 			numeroArquivos = contadorArquivos(listFile);
 			numeroDiretorios = contadorDiretorios(listFile);
-			
-			dir = montarArrayDiretorio(listFile, numeroDiretorios);
-			arq = montarArrayArquivos(listFile, numeroArquivos);
-			
-			resultado = new String[listFile.length + 2];
-			resultado[0] = "list: " + fileDiretorio ;
-			resultado[1] = "total: " + listFile.length;
-			
-			Arrays.sort(dir); 
-			Arrays.sort(arq); 
-			
-			Object[] teste = conteudoDiretorioUnificado(dir, arq);
-			
+
+			arrayDiretorios = montarArrayDiretorio(listFile, numeroDiretorios);
+			arrayArquivos = montarArrayArquivos(listFile, numeroArquivos);
+
+			Arrays.sort(arrayDiretorios);
+			Arrays.sort(arrayArquivos);
+
+			Object[] teste = conteudoDiretorioUnificado(arrayDiretorios,
+					arrayArquivos);
+
 			for (int i = 0; i < teste.length; i++) {
 				resultado[i + 2] = teste[i].toString();
-				//System.out.println(teste[i]);
+				// System.out.println(teste[i]);
 			}
-			
-		} else {
-			resultado = new String[2];
-			resultado[0] = "list: " + fileDiretorio ;
-			resultado[1] = "total: " + listFile.length;
+
 		}
 
 		return resultado;
 	}
 
-	private Object[] conteudoDiretorioUnificado(String[] dir, String[] arq) {
-		// TODO Auto-generated method stub
-		   String[] array1 = dir;
+	private void validaDiretorio(String diretorio) {
+		if (diretorio == null) {
+			throw new IllegalArgumentException();
+		}
 
-           String[] array2 = arq;
+		File fileDiretorio = new File(diretorio);
 
-           List<String> list1 = new ArrayList<String>( Arrays.asList(array1) );
+		if (!fileDiretorio.isDirectory()) {
+			throw new IllegalArgumentException();
+		}
 
-           List<String> list2 = new ArrayList<String>( Arrays.asList(array2) );
+	}
 
-           list1.addAll(list2);
+	private Object[] conteudoDiretorioUnificado(String[] arrayDiretorio,
+			String[] arrayArquivo) {
 
-           
-           
-           return list1.toArray(); 
-		
+		String[] array1 = null;
+		String[] array2 = null;
+		List<String> list1 = null;
+		List<String> list2 = null;
+
+		array1 = arrayDiretorio;
+		array2 = arrayArquivo;
+		list1 = new ArrayList<String>(Arrays.asList(array1));
+		list2 = new ArrayList<String>(Arrays.asList(array2));
+
+		list1.addAll(list2);
+
+		return list1.toArray();
 	}
 
 	private String[] montarArrayArquivos(File[] listFile, int numeroArquivos) {
-		
-		String [] arquivos = null;
+
+		String[] arquivos = null;
 		arquivos = new String[numeroArquivos];
-				
+
 		int cont = 0;
 		for (int i = 0; i < listFile.length; i++) {
 
-			if(listFile[i].isFile()){
-				
+			if (listFile[i].isFile()) {
+
 				arquivos[cont] = "a " + listFile[i].getName();
 				cont++;
-			} 
+			}
 		}
 
 		return arquivos;
 	}
 
 	private String[] montarArrayDiretorio(File[] listFile, int numeroDiretorios) {
-		String []diretorio = null;
+		String[] diretorio = null;
 		diretorio = new String[numeroDiretorios];
-				
+
 		int cont = 0;
 		for (int i = 0; i < listFile.length; i++) {
 
-			if(listFile[i].isDirectory()){
-				
+			if (listFile[i].isDirectory()) {
+
 				diretorio[cont] = "d " + listFile[i].getName();
 				cont++;
-			} 
+			}
 		}
 
-		
 		return diretorio;
 	}
 
@@ -124,24 +128,24 @@ public class ListaDiretorio {
 		int cont = 0;
 		for (int i = 0; i < listFile.length; i++) {
 
-			if(listFile[i].isDirectory()){
+			if (listFile[i].isDirectory()) {
 				cont++;
-			} 
+			}
 		}
-		
+
 		return cont;
-		
+
 	}
 
 	private int contadorArquivos(File[] listFile) {
 		int cont = 0;
 
 		for (int i = 0; i < listFile.length; i++) {
-			if(listFile[i].isFile()){
+			if (listFile[i].isFile()) {
 				cont++;
-			} 
+			}
 		}
-		
+
 		return cont;
 	}
 }
